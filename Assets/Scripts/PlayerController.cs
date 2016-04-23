b796e9dbb63bad587e8df3696d5299f8e5c81958
Kim Blair <kimwb@umich.edu>
2016-04-23 14:23:11 -0400

    basic mobile player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class PlayerController : NetworkBehaviour {
+
+    [SerializeField]
+    private float speed = 5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(isLocalPlayer) {
+            InputMovement();
+        }
+
+
+	}
+
+    void InputMovement() {
+        var x = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        var z = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        transform.Translate(x, 0, z);
+    }
+
+
+}
